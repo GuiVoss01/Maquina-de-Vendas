951ddb3153d9f0a3f69bd2dc870b782b0aef4ec5
Guilherme Voss <guilhermevoss01@gmail.com>
2026-02-04 15:37:00 -0300

    Feat: Adicionada a opção de fazer mais compras limpando o carrinho

--- maquina.py
+++ maquina.py
@@ -5,21 +5,24 @@ produtos = [
     ["Cafe", 1.25, 100],
     ["Redbull", 13.99, 2]
 ]
+
 trocos = [
-    ["20 R$",20,10],
-    ["10 R$",10,10],
-    ["5 R$",5,10],
-    ["2 R$",2,10],
-    ["1 R$",1,10],
-    ["50 centavos",0.5,10],
-    ["25 centavos",0.25,10],
-    ["10 centavos",0.1,10],
-    ["5 centavos",0.05,10],
-    ["1 centavo",0.01,10]
+    ["20 R$", 20, 10],
+    ["10 R$", 10, 10],
+    ["5 R$", 5, 10],
+    ["2 R$", 2, 10],
+    ["1 R$", 1, 10],
+    ["50 centavos", 0.5, 10],
+    ["25 centavos", 0.25, 10],
+    ["10 centavos", 0.1, 10],
+    ["5 centavos", 0.05, 10],
+    ["1 centavo", 0.01, 10]
 ]
+
 listaNomeProdutos = []
 listaPrecoProdutos = []
 listaTroco = []
+
 def selecionarmodo():
     '''Comeco do codigo, o user recebe a possibilidade de exec o modo adm ou comprar'''
     while True:
@@ -31,27 +34,25 @@ def selecionarmodo():
             listaprodutos()
             selecionarprodutos()
             break
-        elif escolha != 1 or escolha!= 123:
+        elif escolha != 1 or escolha != 123:
             print("Senha incorreta")
             continue
-
 
 def listaprodutos():
     '''Mostra a matriz produtos organizada'''
     print("MAQUINA DE VENDAS")
     print("===============================")
     for i, produto in enumerate(produtos):
-    # i é o index, produto acessa a linha
         nome = produto[0]
         preco = produto[1]
         estoque = produto[2]
         print(f'{i + 1}-{produto[0]} - R$ {produto[1]} - estoque {estoque}')
+
 def estoqueNotas():
     '''Estoque de notas'''
     print("Estoque notas")
     print("===============================")
     for i, materia in enumerate(trocos):
-    #funciona igual o anterior
         nome = materia[0]
         preco = materia[1]
         estoque = materia[2]
@@ -65,17 +66,22 @@ def selecionarprodutos():
         except ValueError:
             print("Valor inválido")
             continue
+
         if compra == 0:
             pagamento()
             break
-        if compra <1 or compra>len(produtos):
+
+        if compra < 1 or compra > len(produtos):
             print("ID não identificado")
             continue
-        compraIndex = produtos[compra-1]
+
+        compraIndex = produtos[compra - 1]
         print(f"Você selecionou {compraIndex[0]}")
+
         if compraIndex[2] <= 0:
             print("PRODUTO EM FALTA")
             continue
+
         compraIndex[2] = compraIndex[2] - 1
         listaprodutos()
         listaNomeProdutos.append(compraIndex[0])
@@ -91,28 +97,45 @@ def pagamento():
             valorPagamento = float(input("Valor inserido: "))
         except ValueError:
             print("Valor invalido")
-            valorPagamento = float(input("Valor inserido: "))
+            continue
+
         troco = valorPagamento - sum(listaPrecoProdutos)
         troco = round(troco, 2)
-        if troco ==0:
-            print("Sem troco")
+
         if valorPagamento < sum(listaPrecoProdutos):
             print("Saldo insuficiente")
             continue
-        print(troco)
-        while troco != 0:
-            troco = round(troco, 2)
-            if trocos[n][2] < 0:
-                print("Falta de troco")
-                exit()
-            if troco < trocos[n][1]:
-                n +=1
-            elif troco >= trocos[n][1]:
-                troco = troco - trocos[n][1]
-                trocos[n][2] = trocos[n][2]-1
-                listaTroco.append(n)
+
+        if troco == 0:
+            print("Sem troco")
+        else:
+            while troco != 0:
+                troco = round(troco, 2)
+                if trocos[n][2] < 0:
+                    print("Falta de troco")
+                    exit()
+                if troco < trocos[n][1]:
+                    n += 1
+                else:
+                    troco = troco - trocos[n][1]
+                    trocos[n][2] = trocos[n][2] - 1
+                    listaTroco.append(n)
+
         contagemTroco()
-        break
+
+        while True:
+            opcao = input("\n1 - Fazer nova compra\n2 - Encerrar programa\nEscolha: ")
+            if opcao == '1':
+                listaNomeProdutos.clear()
+                listaPrecoProdutos.clear()
+                listaTroco.clear()
+                listaprodutos()
+                selecionarprodutos()
+                return
+            elif opcao == '2':
+                exit()
+            else:
+                print("Opção inválida")
 
 def contagemTroco():
     '''Basicamente mostra se o print vai ser no plural ou não e mostra a quantidade notas/moedas'''
@@ -120,7 +143,6 @@ def contagemTroco():
     for n in listaTroco:
         if n not in valores_contados:
             quantidade = listaTroco.count(n)
-            #conta quantas vezes n se repete e se não tem n ele add na lista, se n>1 é utilizado o notaS ou moedaS ao contrário não
             valores_contados.append(n)
             if quantidade > 1:
                 if n < 4:
@@ -134,6 +156,7 @@ def contagemTroco():
                     print(f'{quantidade} moeda de {trocos[n][0]}')
     listaprodutos()
     estoqueNotas()
+
 def modoAdm():
     '''Modo adm'''
     while True:
@@ -148,6 +171,7 @@ def modoAdm():
             produtoCadastrado.append(estoquecadastrado)
             produtos.append(produtoCadastrado)
             listaprodutos()
+
         if adm == "editar":
             listaEditar = []
             editar = int(input("qual o ID do produto que você quer editar? "))
@@ -159,13 +183,16 @@ def modoAdm():
             listaEditar.append(estoqueeditado)
             produtos[produtoEditadoIndex][1:3] = listaEditar
             listaprodutos()
+
         if adm == 'remover':
             remover = int(input("qual o id do produto que você quer remover? "))
             produtoremovidoIndex = remover - 1
             produtos.pop(produtoremovidoIndex)
             listaprodutos()
-        if adm =='sair':
+
+        if adm == 'sair':
             listaprodutos()
             selecionarprodutos()
             break
+
 selecionarmodo()
